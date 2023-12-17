14d38e0716fa14983becc483e57ed405ba452698
Matthew Fan <kungfumo@gmail.com>
2023-12-16 16:39:52 -0800

    move alpine logic to js file

--- ui/static/index.js
+++ ui/static/index.js
@@ -1,3 +1,38 @@
+document.addEventListener("alpine:init", () => {
+    Alpine.data("calendar", () => ({
+        selected: [],
+        type: 0,
+        isSelected(d) {
+            return this.selected.includes(d)
+        },
+        addSelected(date) {
+            if (!this.isSelected(date)) {
+                this.selected.push(date)
+            } else {
+                this.selected.splice(this.selected.indexOf(date), 1)
+            }
+        },
+        eventStyle(isCheckIn, date) {
+            style = ""
+            if (this.isSelected(date)) {
+                style = `${style} bg-black text-white`
+            } else if (isCheckIn) {
+                style = `${style} bg-green-300`
+            }
+
+            return style
+        },
+        submitSelected(type) {
+            htmx.ajax('POST', '/events', {
+                values: {
+                    dates: this.selected, 
+                    type: type
+                }
+            })
+        }
+    }))
+})
+
 function formatDate(d) {
     function pad(v) {
         return ("0" + v).slice(-2)
@@ -9,3 +44,5 @@ function formatDate(d) {
 
     return `${year}-${month}-${day}`
 }
+
+
